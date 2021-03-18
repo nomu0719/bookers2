b31b380fbf6c216ac46ceabba537d5c69b6d3e8f
nomu0719 <kei53415@gmail.com>
2021-03-18 10:31:56 +0000

    start

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books.reverse_order
+
+  end
+  def index
+    @users = User.all
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:naame, :profile_image, :introduction)
+  end
+end
